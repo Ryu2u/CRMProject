36c6828c969f4d25834996f1f212cf910467d372
ryuzu <ryuzu@qq.com>
2022-01-14 22:44:55 +0800

    CRM搭建

---
+++ CRM/src/main/java/com/bjpowernode/crm/settings/web/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.crm.settings.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Ryuzu
+ * @date 2022/1/14 22:20
+ */
+@Controller
+public class MyController {
+
+}
